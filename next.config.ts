77aee335e4be55c93265501d440c07b9ee82521d
devSuryansh <suryanshg2050@gmail.com>
2025-03-31 22:25:44 +0530

    Accepts images from host cdn.dribbble.com

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.dribbble.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
